9755df7bfc2016f5dfaf2f7cde1220dfd2a42a8c
Ebola <abolhoseini.sina@gmail.com>
2021-11-23 17:52:14 +0330

    complete connection to recipe-item

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe';
 
 @Component({
   selector: 'rb-recipe-list',
   templateUrl: './recipe-list.component.html',
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [];
+  recipe = new Recipe('Tomato','Red delicious vegetable','https://ponnaorganics.com/wp-content/uploads/2016/02/tomato.jpg')
 
   constructor() { }
 
